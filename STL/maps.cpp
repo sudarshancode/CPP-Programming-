2c4455fdbfeb325b7ac94d37ce5b8a4c4e91a362
sudarshan chakraborty <sudarshanchakraborty420@gmail.com>
2025-03-28 12:01:12 +0530

    Modify maps

--- STL/maps.cpp
+++ STL/maps.cpp
@@ -8,6 +8,30 @@ int main() {
     map<string, int> people = {{"Sudarshan", 20}, {"Akash", 30}};
 
     cout << "Sudarshan is: " << people["Sudarshan"] << endl;
+    cout<<"Akash is:" << people.at("Akash")<<endl;
+
+    people["Ram"]=32;
+    people["Rahul"]=33;
+    people["Shukhdeb"]=40;
+
+    cout<<"Shukhdeb is:"<<people.at("Shukhdeb")<<endl;
+
+    people.insert({"Raktim",39});
+    people.insert({"Rakesh",60});
+
+    cout<<"Raktim is:"<<people.at("Raktim")<<endl;
+
+    //Remove Sudarshan 
+    people.erase("Sudarshan");
+
+    //Iterate
+    cout<<"After removing:"<<endl;
+    for(auto per : people){
+        cout<<per.first<<" is: "<<per.second<<endl;
+    }
+
+    //Clear map
+    people.clear();
 
     return 0;
 }
