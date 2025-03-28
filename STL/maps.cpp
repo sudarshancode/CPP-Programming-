3ab2f11b827c894e8e42b4a39118374fe1ff5229
sudarshan chakraborty <sudarshanchakraborty420@gmail.com>
2025-03-28 11:44:00 +0530

    Add maps

---
+++ STL/maps.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    map<string, int> people = {{"Sudarshan", 20}, {"Akash", 30}};
+
+    cout << "Sudarshan is: " << people["Sudarshan"] << endl;
+
+    return 0;
+}
